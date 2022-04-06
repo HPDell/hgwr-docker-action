14c6b1dc1713508bf5378c46efa3638c717d4f82
HPDell <huyg0180110559@outlook.com>
2022-04-06 18:17:52 +0100

    edit: cmake check script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 mkdir build
 cd build
-cmake .. -DWITH_R=ON
-cmake --build . --config Release --target hgwrr_rbuild
-ctest -R Test_R_hgwr
+cmake .. -DWITH_R=ON && \
+    cmake --build . --config Release --target hgwrr_rbuild && \
+    ctest -R Test_R_hgwr --output-on-failure
